4d555de3889dddd3d48373898bb46857b7c9343c
Matheus_Lacerda <matheus_mtl@hotmail.com>
2022-02-01 13:32:39 -0300

    contacts model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_31_202350) do
+ActiveRecord::Schema.define(version: 2022_02_01_160946) do
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "contact_name"
+    t.date "bith_date"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index "\"name\"", name: "index_contacts_on_name", unique: true
+    t.index ["user_id"], name: "index_contacts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,4 +30,5 @@ ActiveRecord::Schema.define(version: 2022_01_31_202350) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "contacts", "users"
 end
