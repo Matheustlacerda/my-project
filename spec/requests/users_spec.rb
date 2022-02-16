9577011f43bb6df02cccf04a4429a87d63fc10aa
Matheus_Lacerda <matheus_mtl@hotmail.com>
2022-02-16 16:12:40 -0300

    fixes

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -13,4 +13,12 @@ describe "Users", type: :request do
       expect(assigns(:users)).to eq(users)
     end
   end
+
+  describe "GET /show" do
+    it "returns http success" do
+      user = User.create(name:'Jane')
+      get "/users/1"
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
