072c4d2fe28e8f79c59f95636c6c4e97ca36488f
Matheus_Lacerda <matheus_mtl@hotmail.com>
2022-02-17 16:30:28 -0300

    correction

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,45 +1,42 @@
-require 'rails_helper'
-
 describe "Users", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      get "/users"
+      get users_path
       expect(response).to have_http_status(:success)
     end
 
     it "returns a user list" do
       users = User.all
-      get "/users"
+      get users_path
       expect(assigns(:users)).to eq(users)
     end
   end
 
   describe "GET /show" do
     it "returns http success" do
-      user = User.create(name:'Jane')
-      get "/users/1"
+      user = User.create(name:'John')
+      get user_path(user)
       expect(response).to have_http_status(:success)
+    end
+
+    it "returns a single user" do
+      user = User.create(name:'John')
+      get user_path(user)
+      expect(assigns(:user)).to eq(user)
     end
   end
 
   describe "GET /new" do
     it "returns http success" do
-      get "/users/new"
+      get new_user_path
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "Post /create" do
+  describe "Post /create user" do
     it "returns http success" do
-      user = User.create(name:'Jane')
-      get "/users/1"
-      expect(response).to have_http_status(:success)
-    end
-
-    it "returns a user" do
-      user = User.create(name:'Jane')
-      get "/users/1"
-      expect(assigns(:user)).to eq(user)
+      post users_path, params: {user: {name: "Jane"}}
+      expect(response).to have_http_status(:found)
     end
   end
 end
